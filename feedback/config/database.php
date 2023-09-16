49d34291921a863c6a82ffd70fdc0bb94aae367c
Devshara <developer.ishara@gmail.com>
2023-09-16 11:11:14 -0700

    complete feedback form

--- feedback/config/database.php
+++ feedback/config/database.php
@@ -7,6 +7,7 @@
     //create connection
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
+
     //check connection
     if($conn->connect_error){
         die('Connection failed ' . $conn->connect_error);
